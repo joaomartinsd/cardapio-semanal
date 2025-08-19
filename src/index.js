305c7b6d154eae4dffa94bbf12a0dcb7a1288067
João Paulo Martins <59486506+joaomartinsd@users.noreply.github.com>
2025-08-19 14:44:50 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import { createRoot } from "react-dom/client";
 import WeeklyMenuPlanner from "./App";
 import "./index.css";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<WeeklyMenuPlanner />);
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <WeeklyMenuPlanner />
+  </React.StrictMode>
+);
